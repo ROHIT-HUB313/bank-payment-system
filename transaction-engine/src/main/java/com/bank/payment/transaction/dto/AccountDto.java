7263f2f7f08698a6a04d9ad06150c85eabad10b2
ROHIT-HUB313 <engineeringwithrohit@gmail.com>
2025-12-19 15:57:15 +0530

    Transaction dto with json property, added!

--- transaction-engine/src/main/java/com/bank/payment/transaction/dto/AccountDto.java
+++ transaction-engine/src/main/java/com/bank/payment/transaction/dto/AccountDto.java
@@ -1,10 +1,12 @@
 
 package com.bank.payment.transaction.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import java.math.BigDecimal;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AccountDto {
 
     private String accountNumber;
